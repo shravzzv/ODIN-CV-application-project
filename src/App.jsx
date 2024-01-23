75a1c688307a676a8013f1717964fa4fec346cbe
sai shravan <saishravan384@gmail.com>
2024-01-23 10:48:26 +0530

    feat: Add EducationalInfo form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './styles/App.css'
 import GeneralInfo from './components/GeneralInfo'
+import EducationalInfo from './components/EducationalInfo'
 
 function App() {
-  return <GeneralInfo />
+  return (
+    <>
+      <GeneralInfo />
+      <EducationalInfo />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/EducationalInfo.jsx
@@ -0,0 +1,39 @@
+export default function EducationalInfo() {
+  return (
+    <form>
+      <h2 className='form-heading'>Educational Information</h2>
+      <label>
+        School:{' '}
+        <input
+          type='text'
+          name='school'
+          id='school'
+          placeholder='Harvard'
+          required
+        />
+      </label>
+      <label>
+        Course:{' '}
+        <input
+          type='text'
+          name='course'
+          id='course'
+          placeholder='medicine'
+          required
+        />
+      </label>
+      <label>
+        Start date: <input type='date' name='start' id='start' required />
+      </label>
+      <label>
+        End date:
+        <input type='date' name='end' id='end' />
+      </label>
+      <label>
+        Ongoing Studies:
+        <input type='checkbox' name='current' id='current' />
+      </label>
+      <button type='submit'>Submit</button>
+    </form>
+  )
+}
